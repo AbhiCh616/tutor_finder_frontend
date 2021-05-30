d8db9bd75354bc73f50d0def1e4d9fd51c5e7611
Abhi <abhich1607@gmail.com>
2021-05-30 13:04:36 +0530

    BASE_URL placeholder added

--- lib/constants/api_path.dart
+++ lib/constants/api_path.dart
@@ -1,3 +1,3 @@
-const String BASE_URL = '';
+const String BASE_URL = 'http://192.168.1.34:8000/';
 const String CREATE_USER_URL = BASE_URL + 'api/accounts/create/';
 const String LOGIN_USER_URL = BASE_URL + 'api/accounts/login/';
