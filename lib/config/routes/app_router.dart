57a357e517b42987df2fc60212952d14da1915f5
Abhi <abhich1607@gmail.com>
2021-06-20 19:41:29 +0530

    Removed unnecessary page

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/annotations.dart';
 import 'package:tutor_finder_frontend/pages/login/login.dart';
 import 'package:tutor_finder_frontend/pages/signup/signup.dart';
 import 'package:tutor_finder_frontend/pages/splash/splash.dart';
-import 'package:tutor_finder_frontend/pages/tutor_register/tut_reg_basic.dart';
 import 'package:tutor_finder_frontend/pages/welcome/welcome.dart';
 
 @AdaptiveAutoRouter(
@@ -11,7 +10,6 @@ import 'package:tutor_finder_frontend/pages/welcome/welcome.dart';
     AutoRoute(page: WelcomePage, initial: true),
     AutoRoute(page: LoginPage),
     AutoRoute(page: SignupPage),
-    AutoRoute(page: BasicInfoTutorRegPage),
   ],
 )
 class $AppRouter {}
--- lib/pages/tutor_register/tut_reg_basic.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BasicInfoTutorRegPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
